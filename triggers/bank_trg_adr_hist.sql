3c0190cdbe5d7cb2ec0d20a2729d9507e961bf07
Pawel <pawel.pechta@edu.uekat.pl>
2022-08-10 18:41:26 +0200

    fix: typos

--- triggers/bank_trg_adr_hist.sql
+++ triggers/bank_trg_adr_hist.sql
@@ -1,35 +1,35 @@
-create or replace trigger bank_trg_adr_hist
-before update 
-on bank.adresy
-referencing new as new old as old
-for each row
+CREATE OR REPLACE TRIGGER bank_trg_adr_hist
+BEFORE UPDATE 
+ON bank.adresy
+REFERENCING NEW AS NEW OLD AS OLD
+FOR EACH ROW
 
-declare
-    v_kraj_new adresy.kraj%type := :new.kraj;
-    v_miasto_new adresy.miasto%type := :new.miasto;
-    v_ulica_new adresy.ulica%type := :new.ulica;
-    v_nr_bud_new adresy.nr_bud%type := :new.nr_bud;
-    v_nr_miesz_new adresy.nr_miesz%type := :new.nr_miesz;
-    v_kod_pocztowy_new adresy.kod_pocztowy%type := :new.kod_pocztowy;
+DECLARE
+    v_kraj_new adresy.kraj%TYPE := :NEW.kraj;
+    v_miasto_new adresy.miasto%TYPE := :NEW.miasto;
+    v_ulica_new adresy.ulica%TYPE := :NEW.ulica;
+    v_nr_bud_new adresy.nr_bud%TYPE := :NEW.nr_bud;
+    v_nr_miesz_new adresy.nr_miesz%TYPE := :NEW.nr_miesz;
+    v_kod_pocztowy_new adresy.kod_pocztowy%TYPE := :NEW.kod_pocztowy;
 
-begin
+BEGIN
 
-    if :old.kraj = :new.kraj then v_kraj_new := null; end if;
-    if :old.miasto = :new.miasto then v_miasto_new := null; end if;
-    if :old.ulica = :new.ulica then v_ulica_new := null; end if;
-    if :old.nr_bud = :new.nr_bud then v_nr_bud_new := null; end if;
-    if :old.nr_miesz = :new.nr_miesz then v_nr_miesz_new := null; end if;
-    if :old.kod_pocztowy = :new.kod_pocztowy then v_kod_pocztowy_new := null; end if;
+    IF :OLD.kraj = :NEW.kraj THEN v_kraj_new := NULL; END IF;
+    IF :OLD.miasto = :NEW.miasto THEN v_miasto_new := NULL; END IF;
+    IF :OLD.ulica = :NEW.ulica THEN v_ulica_new := NULL; END IF;
+    IF :OLD.nr_bud = :NEW.nr_bud THEN v_nr_bud_new := NULL; END IF;
+    IF :OLD.nr_miesz = :NEW.nr_miesz THEN v_nr_miesz_new := NULL; END IF;
+    IF :OLD.kod_pocztowy = :NEW.kod_pocztowy THEN v_kod_pocztowy_new := NULL; END IF;
 
-    insert into bank.adresy_hist values
-    (       ADR_HIST_ID_SEQ.nextval
+    INSERT INTO bank.adresy_hist VALUES
+    (       ADR_HIST_ID_SEQ.NEXTVAL
         ,   sysdate
-        ,   :old.kraj
-        ,   :old.miasto
-        ,   :old.ulica
-        ,   :old.nr_bud
-        ,   :old.nr_miesz
-        ,   :old.kod_pocztowy
+        ,   :OLD.kraj
+        ,   :OLD.miasto
+        ,   :OLD.ulica
+        ,   :OLD.nr_bud
+        ,   :OLD.nr_miesz
+        ,   :OLD.kod_pocztowy
         ,   v_kraj_new
         ,   v_miasto_new
         ,   v_ulica_new
@@ -37,4 +37,4 @@ begin
         ,   v_nr_miesz_new
         ,   v_kod_pocztowy_new
     );
-end;
+END;
