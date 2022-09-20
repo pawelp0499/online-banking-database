c7bf232b0c2c6f06995948b6821ea0c54b4503aa
Pawel <pawel.pechta@edu.uekat.pl>
2022-09-20 12:28:33 +0200

    fix: changed f_sprawdz_status to private subprogram

--- packages/bank_pckg_transakcja_spec.sql
+++ packages/bank_pckg_transakcja_spec.sql
@@ -1,7 +1,5 @@
 create or replace PACKAGE bank_pckg_transakcja IS
 
-FUNCTION f_sprawdz_status(p_trns_id NUMBER) RETURN BOOLEAN;
-
 PROCEDURE proc_zmien_status(p_trns_id NUMBER);
 
 PROCEDURE proc_zaktualizuj_transaction_logs (p_id_first number, p_id_last number);
