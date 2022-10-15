44f16c0a558c7432ebb15bdd059098cbfca06ace
Pawel <pawel.pechta@edu.uekat.pl>
2022-10-15 21:23:25 +0200

    fix: typos

--- views/bank_vw_klienci_zestawienie_public.sql
+++ views/bank_vw_klienci_zestawienie_public.sql
@@ -1,16 +1,3 @@
-CREATE OR REPLACE VIEW bank.bank_vw_trans_status ("TRNS_ID", "DATA_ZAKS", "DATA_REALIZ", "STATUS") AS
---Author: Pawel
---Version: 1
---Changes:
-    SELECT
-			trns.trns_id
-		,	trns.data_zaks
-		,	trns.data_realiz
-		,	DECODE(trns.data_realiz, NULL, 'NIEZREALIZOWANA', 'ZREALIZOWANA') status
-    FROM transakcje trns
-	WITH READ ONLY;
-	
-	
 create or replace view BANK.BANK_VW_KLIENCI_ZESTAWIENIE_PUBLIC as 
 	SELECT
 		  klient_id
